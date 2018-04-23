b36483aa5352d79c0eac759d4555e886d1cd6566
Dwire <gjdwyer8@gmail.com>
2018-04-23 14:31:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(arr, name) {
+  return arr.filter(driver => driver.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(arr, name) {
+  return arr.filter(driver => driver.substring(0, 1).toUpperCase() === name.substring(0, 1).toUpperCase())
+}
+
+function matchName(arr, name) {
+  return arr.filter(obj => obj.name === name)
+}
